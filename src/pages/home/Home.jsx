f0095b1597cd06bd3d837b0f838062ae73e87467
Ardiant42 <lulukardianto42@gmail.com>
2024-04-03 21:43:03 +0700

    Penugasan Tampilan

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
-//import React from 'react'
 import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widget">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Chart type="order" aspect={2 / 1}/>
+          <Chart type="earning" aspect={2 / 1}/>
+        </div>
+        </div>
+    </div>
   )
 }
 
